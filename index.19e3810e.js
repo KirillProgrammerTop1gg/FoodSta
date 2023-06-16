21e8074945c0396cfe8224b174f04a390e2fbef3
KirillProgrammerTop1gg <68917723+KirillProgrammerTop1gg@users.noreply.github.com>
2023-06-16 16:51:21 +0000

    Deploying to gh-pages from @ KirillProgrammerTop1gg/FoodSta@d291d2ae539d5ebb66b8f197fb5d92a4093a20b4 🚀

--- index.19e3810e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-open-menu]"),closeModalBtn:document.querySelector("[data-close-menu]"),backdrop:document.querySelector("[data-overlay]")};function o(){e.backdrop.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",(function(){console.log("Це клік в бекдроп")}))}();
-//# sourceMappingURL=index.19e3810e.js.map
--- index.8a754e11.js
+++
@@ -1,2 +0,0 @@
-const e={openModalBtn:document.querySelector("[data-open-menu]"),closeModalBtn:document.querySelector("[data-close-menu]"),backdrop:document.querySelector("[data-overlay]")};function o(){e.backdrop.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",(function(){console.log("Це клік в бекдроп")}));
-//# sourceMappingURL=index.8a754e11.js.map
