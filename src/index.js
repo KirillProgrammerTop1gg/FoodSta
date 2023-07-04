81f5471bdbc2ab130f5a0296bc9be5b4145bd177
SpackLubomyr <lubgreen@ukr.net>
2023-07-04 14:13:48 +0300

    Revert "first fix"
    This reverts commit 8b2573604d672fe8c847c6278c353667a9bbacaf.

--- src/index.js
+++ src/index.js
@@ -16,3 +16,15 @@ function logBackdropClick() {
   console.log("Це клік в бекдроп");
 }
 
+const refsOrder = {
+  openOrderBtn: document.querySelector("[data-open-order]"),
+  closeOrderBtn: document.querySelector("[data-close-order]"),
+  backdropOrder: document.querySelector("[data-overlay-order]"),
+};
+
+refs.openOrderBtn.addEventListener("click", toggleOrder);
+refs.closeOrderBtn.addEventListener("click", toggleOrder);
+
+function toggleOrder() {
+  refs.backdropOrder.classList.toggle("is-hidden");
+}
